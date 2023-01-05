020f924e29125aa3598a9c8b0ced52c51802854f
ismaildjanov <umaar.kr@gmail.com>
2023-01-05 23:46:32 +0900

    creating react app and project skeleton

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import './style.css';
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <div className="logo">
+          <img
+            src="logo.png"
+            height="68"
+            width="68"
+            alt="Today I Learned Logo"
+          />
+          <h1>Today I Learned</h1>
+        </div>
+
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+      <main className="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
+  );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts List</section>;
+}
+
+export default App;
